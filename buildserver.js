2b24180d5659834e956f9c10f84e0e6771463a0b
Ajit kumar <kajit0408@gmail.com>
2023-06-03 10:29:10 +0530

    installing external packages

--- buildserver.js
+++ buildserver.js
@@ -1,5 +1,10 @@
 const http=require("http");
-http.createServer((req,resp)=>{
-resp.write("<h1>hello ji</h1>");
-resp.end();
-}).listen(8000,()=>{console.log("running in port 8000")});
+
+const senddata=(req,resp)=>
+{
+    resp.write("<h1>hello ji kya haaal chaal hai </h1>");
+    resp.end();
+}
+http.createServer(senddata).listen(8000,()=>{console.log("running in port 8000")});
+
+//http module handles req and respond on the server
