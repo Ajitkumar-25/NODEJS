22e0225ce6351c119eefaf7e8085e0779bd127a0
Ajit kumar <kajit0408@gmail.com>
2023-06-04 14:22:31 +0530

    input from commandline

---
+++ inputcmdline.js
@@ -0,0 +1,23 @@
+// console.log("hello");
+// console.log(process.argv);
+//creating file from cmdline
+
+// const fs=require('fs');
+// const input=process.argv;
+// fs.writeFileSync(input[2],input[3]);
+
+
+
+//add remove and wrong input
+
+const fs=require('fs');
+const input=process.argv;
+if(input[2]=='add'){  
+    fs.writeFileSync(input[3],input[4]);
+}
+else if(input[2]=='remove'){
+    fs.unlinkSync(input[3]);
+}
+else{
+    console.log("wrong input");
+}
